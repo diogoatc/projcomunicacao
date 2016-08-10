deb8748d9eb8c1997af781c52efbedb0b065f263
Diogo Lopes <diogol_l@hotmail.com>
2016-08-09 21:55:17 -0300

    troca do servidor mysql

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor = 'localhost';
-	$usuario = 'root';
-	$senha = '';
-	$banco = 'projcomunicacao';
+$servidor = 'sql3.freemysqlhosting.net:3306';
+	$usuario = 'sql3130819';
+	$senha = 'P9fRUQZbwv';
+	$banco = 'sql3130819';
 	// Conecta-se ao banco de dados MySQL
 		$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
 
