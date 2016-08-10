9a5c55c2939382fe99de7775f5a6ae6235578c5f
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2016-08-09 21:50:39 -0300

    - Troca da imagem - alterações e correções nos inputs

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
-<html lang="en">
+<html lang="pt-br">
 
 <head>
+    <title>UNASP Comunicação</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="header">
-            <img src="assets/img/Comunica%C3%A7%C3%A3o.png" alt="logo unasp">
+            <img src="assets/img/UNASP.png" alt="logo unasp">
         </div>
 
         <div class="content">
             <div class="login">Login</div>
             <div class="form">
                 <form action="validacao.php" method="post">
-                    <input type="text" name="usuario" placeholder="Usuario">
-                    <input type="password" name="senha" placeholder="Senha">
+                  <input type="email" name="" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'">
+                  <input type="password" name="" placeholder="Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Senha'">
                 <br/>
                 <button class="button">Enviar</button>
                 </form>
