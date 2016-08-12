2ba480684907fd529c5e258d13eed46f389ab5dc
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2016-08-11 23:33:59 -0300

    Correções para testes

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']) OR empty($_POST['nome']) OR empty($_POST['email']))) {
-	
+
 	header("Location: index.php"); exit;
 
 }
@@ -13,14 +13,17 @@ $senha = mysqli_real_escape_string($mysqli, sha1($_POST['senha']));
 $nome = mysqli_real_escape_string($mysqli, $_POST['nome']);
 $email = mysqli_real_escape_string($mysqli, $_POST['email']);
 $id=null;
+$nivel = 2;
 
 $stmt = $mysqli->prepare("INSERT INTO usuarios (id,nome,usuario,senha,email,nivel) VALUES(?, ?, ?, ?, ?, ?)");
-$stmt->bind_param('issssi', $id, $nome, $usuario, $senha, $email, "2");
+$stmt->bind_param('issssi', $id, $nome, $usuario, $senha, $email, $nivel);
+
 if($stmt->execute()){
-    print "Sucesso!";
+    echo "Sucesso!";
 }else{
     die('Error : ('. $mysqli->errno .') '. $mysqli->error);
 }
+
 $stmt->close();
 
 ?>
--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-$servidor = 'sql3.freemysqlhosting.net:3306';
+	$servidor = 'sql3.freemysqlhosting.net:3306';
 	$usuario = 'sql3130819';
 	$senha = 'P9fRUQZbwv';
 	$banco = 'sql3130819';
 	// Conecta-se ao banco de dados MySQL
-		$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+	$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
 
-    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+  if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 
-    ?>
+?>
