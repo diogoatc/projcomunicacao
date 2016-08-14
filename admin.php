b5ab4511bfdad83e18bdb413075362aa6afb3064
diogoatc <diogol_l@hotmail.com>
2016-08-14 18:45:51 -0300

    Criação de função Logout
    E pequenas alterações na session

--- admin.php
+++ admin.php
@@ -3,11 +3,12 @@
     // A sessão precisa ser iniciada em cada página diferente
     if (!isset($_SESSION)) session_start();
       
-    $nivel_necessario = 1;
+    $nivel_necessario = 1;  //1 é o nível Admin
       
     // Verifica se não há a variável da sessão que identifica o usuário
     if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] !=$nivel_necessario)) {
         // Destrói a sessão por segurança
+        echo "<script> alert('Você precisa estar logado para acessar essa página');</script>";
         session_destroy();
         // Redireciona o visitante de volta pro login
         header("Location: index.php"); exit;
@@ -19,4 +20,6 @@
     <p>Olá, <?php echo $_SESSION['UsuarioNome']; ?>!</p>
     <br/>
     <h3><a href="cadastro.php"> Fazer cadastro de novo Professor </a></h3>
+     <h3><a href="logout.php"> SAIR </a></h3>
+
 
--- professor.php
+++ professor.php
@@ -3,10 +3,11 @@
     // A sessão precisa ser iniciada em cada página diferente
     if (!isset($_SESSION)) session_start();
       
-    $nivel_necessario = 2;
+    $nivel_necessario = 2;  // 2 é o nível Professor
       
     // Verifica se não há a variável da sessão que identifica o usuário
     if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] !=$nivel_necessario)) {
+         echo "<script> alert('Você precisa estar logado para acessar essa página');</script>";
         // Destrói a sessão por segurança
         session_destroy();
         // Redireciona o visitante de volta pro login
@@ -17,5 +18,7 @@
       
     <h1>Página restrita</h1>
     <p>Olá, <?php echo $_SESSION['UsuarioNome']; ?>!</p>
+
+    <h3><a href="logout.php"> SAIR </a></h3>
 
 
