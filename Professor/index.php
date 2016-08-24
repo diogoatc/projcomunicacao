6c677c311376c2229016087773b9d8cfb575a236
WesleyRTI <wesley_93wr@hotmail.com>
2016-08-24 00:28:39 -0300

    Criação do Menu - tela professor
    Criei um menu na área do professor.

---
+++ Professor/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Área do Professor</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+
+<div class="container">
+    <div class="header">
+        <img src="../assets/img/UNASP.png" alt="logo unasp">
+
+        <nav id="menu">
+            <h1>Menu Principal</h1>
+            <ul type="disc">
+                <li><a href="cadastraquestoes.php">Cadastrar Questões</a></li>
+                <li><a href="notasalunos.php">Notas Alunos</a></li>
+                <li><a href="alterarsenha.php">Alterar Senha</a></li>
+
+            </ul>
+        </nav>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ Professor/notasalunos.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WesleyRTI
+ * Date: 24/08/2016
+ * Time: 00:25
+ */
