401111a1c47e32b367d4f647d16cb2a9bdf38392
wesleyrti <wesley_93wr@hotmail.com>
2016-08-25 20:41:56 -0300

    Update - tela alterar senha
    update

--- Professor/alterarsenha.php
+++ Professor/alterarsenha.php
@@ -27,10 +27,10 @@
         <div class="content">
             <div class="login">Alterar Senha</div>
             <div class="form">
-                <form action="validacao.php" method="post">
-                    <input type="password" name="senha" required="Digite a senha atual" placeholder="Digite a Senha Atual" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Digite a senha atual'">
-                    <input type="password" name="senha" required="Digite nova senha" placeholder="Digite a Nova Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Digite nova senha'">
-                    <input type="password" name="senha" required="Confirme a senha" placeholder="Confirme a Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Confirme a senha'">
+                <form action="#" method="post">
+                    <input type="password" name="senhaatual" required="Digite a senha atual" placeholder="Digite a Senha Atual" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Digite a senha atual'">
+                    <input type="password" name="novasenha" required="Digite nova senha" placeholder="Digite a Nova Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Digite nova senha'">
+                    <input type="password" name="confsenha" required="Confirme a senha" placeholder="Confirme a Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Confirme a senha'">
                     <br/>
                     <button class="button">OK</button>
                 </form>
