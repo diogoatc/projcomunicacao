bb793cdf98eac3513f12c9d9e45f321a641d5e48
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2016-08-30 23:33:43 -0300

    Adicionados campos certos no formulário do aluno

--- aluno/index.php
+++ aluno/index.php
@@ -10,9 +10,24 @@
         <form class="" action="aluno.php" method="post">
           <input type="text" name="nome" value="" placeholder="Nome"><br>
           <input type="number" name="ra" value="" placeholder="RA"><br>
-          <input type="text" name="semestre" value="" placeholder="Semestre"><br>
-          <input type="text" name="curso" value="" placeholder="Curso"><br>
-          <input type="text" name="turno" value="" placeholder="Turno">
+          <select required="" name="semestre" id="semestre">
+    				<option value="1">1º Semestre</option>
+    				<option value="2">2º Semestre</option>
+    				<option value="3">3º Semestre</option>
+    				<option value="4">4º Semestre</option>
+    				<option value="5">5º Semestre</option>
+    				<option value="6">6º Semestre</option>
+    				<option value="7">7º Semestre</option>
+    				<option value="8">8º Semestre</option>
+    			</select><br>
+          <select name="curso" id="curso">
+        		<option value="PP">PP</option>
+        		<option value="RTV">RTV</option>
+      		</select><br>
+          <select name="turno" id="turno">
+        		<option value="Matutino">Matutino</option>
+        		<option value="Noturno">Noturno</option>
+      		</select>
           <input type="submit" name="fazerprova" value="Fazer prova">
         </form>
       </div>
