b2a6f589035d8a5c5db6ac7d3196639c8118d899
diogoatc <diogol_l@hotmail.com>
2016-09-07 17:25:38 -0300

    Inserção de imagem em fase de testes

---
+++ testes/testeimagem.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste Imagem</title>
+</head>
+<body>
+	<form enctype="multipart/form-data" action="testeimagem.php" method="post" >
+	<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+		<p>IMAGEM:</p>
+		<input type="file" name="imagem"> <br/>
+		<input type="submit" name="envia">
+	</form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['envia'])){
+
+	$imagem = $_FILES["imagem"];
+	$img_src = $imagem['tmp_name'];
+	$imgbinary = fread(fopen($img_src, "r"), filesize($img_src));
+	$img_str = base64_encode($imgbinary);
+	echo '<img src="data:image/jpg;base64,'.$img_str.'" />';
+
+	
+
+}
+
+
+?>
