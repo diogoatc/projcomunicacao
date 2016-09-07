a50cfbe592aa35b95aed390e05a61a7f0f30ffd8
diogoatc <diogol_l@hotmail.com>
2016-09-07 17:39:31 -0300

    continuação dos testes com a foto

--- testes/testeimagem.php
+++ testes/testeimagem.php
@@ -5,9 +5,9 @@
 </head>
 <body>
 	<form enctype="multipart/form-data" action="testeimagem.php" method="post" >
-	<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+	<input type="hidden" name="MAX_FILE_SIZE" value="300000" />
 		<p>IMAGEM:</p>
-		<input type="file" name="imagem"> <br/>
+		<input type="file" data-max-size="2048" name="imagem"> <br/>
 		<input type="submit" name="envia">
 	</form>
 </body>
@@ -18,6 +18,8 @@
 if(isset($_POST['envia'])){
 
 	$imagem = $_FILES["imagem"];
+	echo $imagem['error'];
+	var_dump($imagem);
 	$img_src = $imagem['tmp_name'];
 	$imgbinary = fread(fopen($img_src, "r"), filesize($img_src));
 	$img_str = base64_encode($imgbinary);
