92436e5bc869dd35b33cbdc54b6fcce3057041b6
WesleyRTI <wesley_93wr@hotmail.com>
2016-09-16 12:15:03 -0300

    Mudança de Nav-bar

--- Professor/cadastraquestoes.php
+++ Professor/cadastraquestoes.php
@@ -9,49 +9,50 @@
 	<head>
 		<title>Cadastro de Questões</title>
 		<meta charset="utf-8">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <script src="../assets/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+	    <script src="../assets/bootstrap-3.3.7-dist/js/newjs.js"></script>
 	      <link rel="stylesheet" href="../assets/css/normalize.css">
 	      <link rel="stylesheet" href="../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	      <link rel="stylesheet" href="../assets/bootstrap-3.3.7-dist/js/newjs.js">
-
 	      <link rel="stylesheet" href="../assets/css/newstyle.css">
-	       <!-- Latest compiled and minified CSS -->
-	      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-	      <!-- Optional theme -->
-	      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	      <!-- Latest compiled and minified JavaScript -->
-	      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 
 	<body>
 
-		<nav class="navbar navbar-inverse" style="border-radius:0px; background:#20205a;">
+<nav class="navbar navbar-inverse" style="border-radius:0px; background:#20205a;>
 
-
-	    <div class="container-fluid">
-
+  <div class="container-fluid">
+    
 	      <div class="col-sm-2">
 	        <a  class="navbar-brand" href="index.php"><img style="margin-top:-13px;width:70%;"  src="../assets/img/UNASP.png" alt="logo unasp"></a>
-	      </div>
+	     </div>
+          
 	      <div class="col-sm-3">
 	        <h3 class="areadoprofessor">ÁREA DO PROFESSOR</h3>
 	      </div>
-	      <div class="col-sm-7">
-	      <ul class="nav navbar-nav">
-					<li><a id="font-white" href="index.php">Home</a></li>
-	        <li class="active"><a id="ativo" href="pre-cadastra.php">Cadastrar Questões</a></li>
-	        <li><a id="font-white" href="relatorios.php">Relatório de Provas</a></li>
-	        <li><a id="font-white" href="listadisciplinas.php">Editar/Visualizar Questões</a></li>
+
+    <div class="col-sm-7">
+    <ul class="nav navbar-nav">
+      <li class="active"><a id="ativo" href="index.php">Home</a></li>
+<li><a id="font-white" href="pre-cadastra.php">Cadastrar Questões</a></li>
+      <li class="dropdown"><a id="font-white" class="dropdown-toggle" data-toggle="dropdown" href="#">Relatório de Prova<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a target="_blank" href="../classes/class_relatorio_disciplinas.php?idusuario=<?php echo $_SESSION['UsuarioID'] ?>">
+                    Relatorios de todas as disciplinas
+                </a></li>
+           </ul>
+      </li>
+      <li><a id="font-white" href="listadisciplinas.php">Editar/Visualizar Questões</a></li>
 	        <li><a id="font-white" href="alterarsenha.php">Alterar Senha</a></li>
 	        <li><a id="font-white" href="../logout.php">Logout</a></li>
-	      </ul>
-	      </div>
-	      </div>
-	  </nav>
+    </ul>
+  </div>
+  </div>
+</nav>
 
 	<legend class="text-center">Cadastrar de Questão</legend>
 
@@ -168,8 +169,18 @@
 
 	</fieldset>
 	</form>
+	
+<div id="push"></div>
+    </div>
+ 		 <div id="footer">
+      <div class="container">
+        <p class="muted credit"> Unasp - Centro Universitário Adventista de São Paulo - © 2016 - Todos os direitos reservados.</a></p>
+      </div>
+    </div>
+	</body>
+	</html>
 
-	<?php
+<?php
 
 
 	if(isset($_POST['envia'])){
@@ -217,15 +228,4 @@
 		}
 	}
 
-
-	?>
-	
-<div id="push"></div>
-    </div>
- 		 <div id="footer">
-      <div class="container">
-        <p class="muted credit"> Unasp - Centro Universitário Adventista de São Paulo - © 2016 - Todos os direitos reservados.</a></p>
-      </div>
-    </div>
-	</body>
-	</html>
+?>
--- testes/testemenu.php
+++ testes/testemenu.php
@@ -1,12 +1,12 @@
-  <?php
-  include('verifica_sessao_professor.php');
-  ?>
+<?php
+include('verifica_sessao_professor.php');
+?>
 
-  <!DOCTYPE html>
-
-  <head>
-      <title>Área do Professor</title>
-      <meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Relatórios</title>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,10 +17,9 @@
 	 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	      <link rel="stylesheet" href="../assets/bootstrap-3.3.7-dist/js/newjs.js">
 	      <link rel="stylesheet" href="../assets/css/newstyle.css">
-  
-  </head>
-
-  <body>
+          
+</head>
+<body>
 
 <nav class="navbar navbar-inverse" style="border-radius:0px; background:#20205a;>
 
@@ -36,7 +35,7 @@
 
     <div class="col-sm-7">
     <ul class="nav navbar-nav">
-      <li class="active"><a id="ativo" href="index.php">Home</a></li>
+      <li class="active"><a id="ativo" href="#">Home</a></li>
 <li><a id="font-white" href="pre-cadastra.php">Cadastrar Questões</a></li>
       <li class="dropdown"><a id="font-white" class="dropdown-toggle" data-toggle="dropdown" href="#">Relatório de Prova<span class="caret"></span></a>
         <ul class="dropdown-menu">
@@ -52,28 +51,19 @@
   </div>
   </div>
 </nav>
-  <div id="wrap">
-    <div class="geral">
-  <div class="container">
-      <div class="row">
-        <h1 class="text-center">Seja bem-vindo professor(a): <?php echo $_SESSION['UsuarioNome'];?></h1>
-        
-          </div>
-        </div> 
-  </div>
-
-
+<div id="wrap">
+  <h3>Navbar With Dropdown</h3>
+  <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
 </div>
-<div id="push"></div>
-    </div>
 
-    <div id="footer">
+  <div id="push"></div>
+    </div>
+ 		 <div id="footer">
       <div class="container">
         <p class="muted credit"> Unasp - Centro Universitário Adventista de São Paulo - © 2016 - Todos os direitos reservados.</a></p>
       </div>
     </div>
 
-   </body>
-  </html>
-
+</body>
+</html>
 
