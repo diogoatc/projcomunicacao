02e84af87ad8767e3f4cc6360ab24e35147d30da
diogoatc <diogol_l@hotmail.com>
2016-09-16 15:47:05 -0300

    Finalizacao Back visualizaquestão

--- Professor/visualizaquestao.php
+++ Professor/visualizaquestao.php
@@ -23,14 +23,15 @@
       <br>
 
             <form>
-              <input type="radio" name="respQuestao<?php echo $numResposta; ?>" value="A" required><?php print_r($key[0]['resposta1']) ?><br>
-              <input type="radio" name="respQuestao<?php echo $numResposta; ?>" value="B"><?php print_r($key[0]['resposta2']) ?><br>
-              <input type="radio" name="respQuestao<?php echo $numResposta; ?>" value="C"><?php print_r($key[0]['resposta3']) ?><br>
-              <input type="radio" name="respQuestao<?php echo $numResposta; ?>" value="D"><?php print_r($key[0]['resposta4']) ?><br>
-              <input type="radio" name="respQuestao<?php echo $numResposta; ?>" value="E"><?php print_r($key[0]['resposta5']) ?><br>
+              <input type="radio" required><?php print_r($key[0]['resposta1']) ?><br>
+              <input type="radio" value="B"><?php print_r($key[0]['resposta2']) ?><br>
+              <input type="radio"  value="C"><?php print_r($key[0]['resposta3']) ?><br>
+              <input type="radio"  value="D"><?php print_r($key[0]['resposta4']) ?><br>
+              <input type="radio" value="E"><?php print_r($key[0]['resposta5']) ?><br>
            <form>
 
-           <a href="listaquestoes.php?id="<? echo $iddisciplina;?>"> aaaaaa </a>
+           <a href="listaquestoes.php?id=<?php echo $iddisciplina;?>"> Voltar para a Lista de Questões </a>
+           
 </body>
 </html>
 
