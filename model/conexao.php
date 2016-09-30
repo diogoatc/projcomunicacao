8b63b2e69e4bd789e199ef607acfce3e5e5651b9
diogoatc <diogol_l@hotmail.com>
2016-09-30 11:20:10 -0300

    alteracao do bd para testes

--- model/conexao.php
+++ model/conexao.php
@@ -1,17 +1,17 @@
 <?php
 //Servidor de produção
+/*
 $host = "169.44.117.18:3306";
 $user ="root";
 $pswrd = "unasp";
 $dbname = "projcomunicacao";
-
+*/
 //Servidor de testes
-/*
 $host = "us-cdbr-iron-east-04.cleardb.net";
 $user ="b74daafaa1c67b";
 $pswrd = "30316046";
 $dbname = "ad_ef0ae188db8c966";
-*/
+
 //Servidor Local
 /*
 $host = "127.0.0.1:3306";
