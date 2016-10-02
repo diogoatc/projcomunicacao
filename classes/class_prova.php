d7e85a25b62332a43577c52d50a833056a47db1b
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2016-10-01 22:30:53 -0300

    Correção salvar prova

--- classes/class_prova.php
+++ classes/class_prova.php
@@ -51,15 +51,16 @@ class prova {
 	}
 
 	function salvarProva($pdo,$ra,$nomealuno,$nota,$dtainicio,$disciplinas,$idquestoes,$respostaAluno){
+		date_default_timezone_set('America/Sao_Paulo');
+		$dtafim = date('Y-m-d H:i:s');
+
 		$conn = $pdo->prepare("INSERT INTO prova (ra,nomealuno,nota,dtainicio,dtafim)
-		VALUES (:ra, :nomealuno, :nota, :dtainicio, :dtafim ");
+		VALUES (:ra, :nomealuno, :nota, :dtainicio, :dtafim)");
 
 		$conn->bindParam(":ra", $ra, PDO::PARAM_INT);
 		$conn->bindParam(":nomealuno", $nomealuno, PDO::PARAM_STR);
 		$conn->bindParam(":nota", $nota, PDO::PARAM_STR);
 		$conn->bindParam(":dtainicio", $dtainicio, PDO::PARAM_STR);
-		date_default_timezone_set('America/Sao_Paulo');
-		$dtafim = date('Y-m-d H:i:s');
 		$conn->bindParam(":dtafim", $dtafim, PDO::PARAM_STR);
 
 		if($conn->execute()){
