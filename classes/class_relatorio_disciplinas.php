56b7a3cd33279fa751fc1bb38f507e2f84d015e9
Diogo Lopes <diogoatc@unasp.net>
2016-10-25 12:28:30 -0200

    testes com os relatórios

--- classes/class_relatorio_disciplinas.php
+++ classes/class_relatorio_disciplinas.php
@@ -4,16 +4,12 @@
       $html = '
       <link rel="license" href="../assets/reports/css/style.css">
       <header class="clearfix">
-        <h1 style="">Relatório de todas as Disciplinas</h1>
+        <h1 style="">Relatório de Notas</h1>
       </header>
       <main>
         <table>
           <thead>
             <tr>
-              <th class="desc" colspan="4">Disciplina</th>
-              <th class="desc" colspan="4">Curso</th>
-              <th class="desc" colspan="4">Turno</th>
-              <th class="desc" colspan="4">Semestre</th>
               <th class="desc" colspan="4">RA</th>
               <th class="desc" colspan="4">Aluno</th>
               <th class="desc" colspan="4">Nota</th>
@@ -22,26 +18,24 @@
           <tbody>';
           include '../model/conexao.php';
           $idusuario = $_GET['idusuario'];
-          $consulta = $PDO->query("SELECT D.nome, D.curso, D.turno, D.semestre,P.ra, P.nomealuno, ROUND(P.nota, 1) AS nota
+          $curso = "PP";
+          $turno = "Noturno";
+          $semestre = "1";
+          $consulta = $PDO->prepare("SELECT P.ra, P.nomealuno, ROUND(P.nota, 1) AS nota
                                     FROM disciplina D
                                     INNER JOIN prova_disciplina PD
                                     ON D.id = PD.iddisciplina
                                     INNER JOIN prova P
-                                    ON P.id = PD.idprova WHERE D.idusuario = 2");
+                                    ON P.id = PD.idprova WHERE D.curso=:curso AND D.turno=:turno AND D.semestre=:semestre ORDER BY P.nomealuno ASC");
+            $consulta->bindParam(":curso",$curso,PDO::PARAM_STR);
+            $consulta->bindParam(":turno",$turno,PDO::PARAM_STR);
+            $consulta->bindParam(":semestre",$semestre,PDO::PARAM_STR);
             $consulta->execute();
             $resultado=$consulta->fetchAll(PDO::FETCH_ASSOC);
 
              foreach ($resultado as $linha) {
               $html .= '
               <tr>';
-                $html .= '
-                <td class="desc" colspan="4">'.$linha['nome'].'</td>';
-                $html .= '
-                <td class="desc" colspan="4">'.$linha['curso'].'</td>';
-                $html .= '
-                <td class="desc" colspan="4">'.$linha['turno'].'</td>';
-                $html .= '
-                <td class="desc" colspan="4">'.$linha['semestre'].'</td>';
                 $html .= '
                 <td class="desc" colspan="4">'.$linha['ra'].'</td>';
                 $html .= '
@@ -57,9 +51,9 @@
       </main>';
 
   $pdf = new mPDF('utf-8', 'A4');
-  $file = "teste.pdf";
-  $pdf->SetHeader('unasp@net.com|Relatorio_Teste|{PAGENO}');
-  $pdf->SetFooter('Sistema de gerenciamento@2016||{DATE j-m-Y}');
+  $file = "relatorio_notas.pdf";
+  $pdf->SetHeader('Prova Unificada|Relatório de notas|{PAGENO}');
+  $pdf->SetFooter('Turma de Sistemas para Internet@2016||{DATE j-m-Y}');
   $pdf->WriteHTML($html);
   $md = strcode2utf($file);
   $pdf->SetTitle($md);
---
+++ relatorios/relatorio_teste.php
@@ -0,0 +1,1 @@
+relatorio_teste.php
