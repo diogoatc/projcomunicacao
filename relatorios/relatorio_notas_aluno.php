851b2be77f2b9dd137439607597fcafb18c9fbce
diogoatc <diogol_l@hotmail.com>
2017-05-09 18:34:54 -0300

    testes

--- relatorios/relatorio_notas_aluno.php
+++ relatorios/relatorio_notas_aluno.php
@@ -2,8 +2,8 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 $dataatual = date('Y-m-d');
-$datainicial= strtotime('2017-05-26'); // RTV
-$datafinal= strtotime('2017-05-28');  // RTV
+$datainicial= strtotime('2017-05-05'); // RTV
+$datafinal= strtotime('2017-05-20');  // RTV
 /*
 $datainicial= strtotime('2017-06-02'); // PP
 $datafinal= strtotime('2017-06-04');  // PP
